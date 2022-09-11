fd85c5d086f94bebc945feeecb948270960faa6e
Miguigg <miguigg1412@gmail.com>
2022-09-11 16:28:16 +0200

    hola

--- My_App08/app/src/main/java/com/example/my_app08/MainActivity.java
+++ My_App08/app/src/main/java/com/example/my_app08/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.my_app08;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText et1,et2;
+    Spinner s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et1 = findViewById(R.id.et1);
+        et2 = findViewById(R.id.et2);
+        s = findViewById(R.id.spinner);
+        String [] op = {"Suma","Resta","Multiplicar"};
+        ArrayAdapter<String>adptador=new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,op);
+        s.setAdapter(adptador);
+    }
+    public void calculo(View v){
+        Intent intent = new Intent(this,actv_2.class);
+        int v1 = Integer.parseInt(et1.getText().toString());
+        int v2 = Integer.parseInt(et2.getText().toString());
+        String operacion = s.getSelectedItem().toString();
+        intent.putExtra("valor1",v1);
+        intent.putExtra("valor2",v2);
+        intent.putExtra("operacion",operacion);
+        startActivity(intent);
     }
 }
--- My_App08/app/src/main/java/com/example/my_app08/actv_2.java
+++ My_App08/app/src/main/java/com/example/my_app08/actv_2.java
@@ -3,12 +3,28 @@ package com.example.my_app08;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class actv_2 extends AppCompatActivity {
-
+    TextView t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actv2);
+        t1 = findViewById(R.id.textView);
+        Bundle datos = getIntent().getExtras();
+        int v1 = datos.getInt("valor1");
+        int v2 = datos.getInt("valor2");
+        String op = datos.getString("operacion");
+        int toret;
+        switch (op){
+            case "Suma": toret = v1+v2; t1.setText("Resultado: "+toret); ;break;
+            case "Resta":toret = v1-v2;t1.setText("Resultado: "+toret);break;
+            case "Multiplicar":toret = v1*v2;t1.setText("Resultado: "+toret);break;
+        }
+    }
+    public void volver(View v){
+        finish();
     }
 }
